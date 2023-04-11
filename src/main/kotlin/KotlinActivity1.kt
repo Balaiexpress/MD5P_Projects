03c32fc2b36f1911a20db9c35f3f42b98a77a7dc
Bernardo Padilla jr <balaiexpressdelivery@gmail.com>
2023-04-11 18:59:20 +0800

    test

---
+++ src/main/kotlin/KotlinActivity1.kt
@@ -0,0 +1,71 @@
+fun main(){
+    // Application for Employment
+    var position:String = "Mobile Dev"
+    var availDate:String = "Anytime"
+    var desiredSalary:Int = 50000
+
+    //Personal Information
+    var lastName:String = "Padilla Jr"
+    var firsName:String = "Bernardo"
+    var middleName:String = "Pracillo"
+
+
+    var fullAdd:String = "Santa Rosa"
+    var city:String = "Bayombong"
+    var state:String = "Nueva Vizcaya"
+    var zipCode:Int = 3700
+
+
+    var socialSecNum:Int = 154748000
+    var citizenstatus:Boolean = false
+    var convictionStatus: Boolean = false
+    var submitDrugTest: Boolean = false
+
+
+
+    //EDUCATION
+    var shoolname1:String = "Calitlitan Elementary School"
+    var locshoolname1:String = "Calitlitan Aritao Nueva Vizcaya"
+    var year1:String = "2002"
+    var schoolname2:String = "Aritao National Highschool"
+    var locschoolname2:String = "Aritao Nueva Vizcaya"
+    var year2:String = "2006"
+    var schoolname3:String = "Nueva Vizcaya State University"
+    var locshoolname3:String = "Bayombong Nueva Vizcaya"
+    var year3:String = "2010"
+    var degree:String = "BSEE"
+
+    //EMPLOYMENT
+    var employer:String = "MDP WORLD INC"
+    var dateemp:String = "January 20,2019"
+    var emplNum:Int = 956664430
+    var emprate:Double = 15000.0
+    var empFullAdd:String = "Cabanatun City Nueva Ecija"
+    var empPosition:String = "Marketing Manager"
+
+    //Reference
+    var refName:String = "Arwin Panaligan"
+    var refTitle:String = "CEO"
+    var refCampany:String = "MDPWORLD INC"
+    var refContact:String = "09557225420"
+
+    println("Position You Are Applying for: $position")
+    println("Desired Salary: $desiredSalary")
+    println("Date Available For Work: $availDate")
+    println("Last Name : $lastName")
+    println("First Name : $firsName")
+    println("Middle Name : $middleName")
+    println("Address : $fullAdd")
+    println("City : $city")
+    println("State : $state")
+    println("Zipcode : $zipCode")
+
+    //
+
+
+
+
+
+
+
+}
---
+++ src/main/kotlin/KotlinProgram2.kt
@@ -0,0 +1,15 @@
+fun main (){
+    var name:String = "John Weak"
+    println("Hello $name")
+
+    var age:Int = 25
+    var salary:Double = 1000.0
+    var skillLevel:Char = 'S'
+    var isActive:Boolean = true
+    println("His Age is $age")
+    println("His salary is $salary")
+    println("His Skill Level is $skillLevel")
+    println("Is he active? $isActive")
+
+
+}
